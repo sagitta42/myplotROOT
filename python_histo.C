bdd50e6b89f2d5934f54256700b003f60469459d
sagitta42 <sssagitta@gmail.com>
2019-08-16 11:42:02 +0200

    macro to save histo as txt for python

--- python_histo.C
+++ python_histo.C
@@ -1,11 +1,22 @@
-int main(){
+#include <string>
+#include "TFile.h"
+#include "TList.h"
+#include "TKey.h"
+#include <iostream>
+#include "TH1F.h"
+#include <fstream>
+
+using namespace std;
+
+int main(int argc, char* argv[]){
     /* Save C histograms as txt files */
 
     // given file
     string filename = argv[1];
+    TFile f(filename.c_str());
 
     // list of histos in given file
-	TList* list = f->GetListOfKeys();
+	TList* list = f.GetListOfKeys();
 	TIter next(list);
 	TKey* key;
 	
@@ -14,18 +25,22 @@ int main(){
 
 	while( (key = (TKey*)next()) ){
         // get histo
-		TH1F* h = (TH1D*) key->ReadObj();
+		TH1D* h = (TH1D*) key->ReadObj();
 
         // file to which to save info
         char outname[30];
         sprintf(outname, "histo_%s.txt", h->GetName());
+        cout << outname << endl;
         ofstream out(outname);
 
         // loop over each bin and save info
-        int nbins = h->GetNBins();
+        int nbins = h->GetNbinsX();
+        for(int b = 0; b < nbins; b++) out << h->GetBinCenter(b) << " " << h->GetBinContent(b) << endl;
 
-        for(int b = 0; b < nbins; b++) out << b->GetBinCenter(b) << " " << b->GetBinContent(b) << endl;
+        out.close();
 
     }
+
+    f.Close();
 
 }
